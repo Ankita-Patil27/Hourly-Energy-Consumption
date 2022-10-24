5f0f074ba268ef103e28e163475882e8b4c8df7e
Ankita Patil <81069934+Ankita-Patil27@users.noreply.github.com>
2022-10-24 15:02:30 +0530

    Add files via upload

---
+++ Main_File.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import datetime
+
+st.title("Welcome to Energy Consumption Project")
+
+
+st.sidebar.header('User Input Parameters')
+
+no_days = st.sidebar.number_input("Insert Number of days", min_value=1, max_value=365, step=1)
+#days = {'Days': no_days}
+#features = pd.DataFrame(days, index=[0])
+
+
+
+#df = user_input_features()
+#st.subheader('User Input parameters')
+#st.write(features)
+
+
+with open("Linear_Regression_Final_Model.pkl", mode="rb") as f:
+    model = pickle.load(f)
+
+data=pd.read_csv('Daywise Consumption Data.csv',index_col='Datetime',parse_dates=True)
+data.rename({'PJMW_MW':'MW'},inplace=True,axis=1)
+forecast_check_data = np.array(data['MW'][:'2018-07-04'][-7:])
+z=forecast_check_data
+
+for i in range(0,no_days):
+    ck=z[-7:]
+    ck=np.array([ck])
+    lin_f_chk=model.predict(ck)
+    z=np.append(z,lin_f_chk)
+    i=+1
+future_pred_lr=z[-no_days:]
+
+ct = datetime.datetime.now() + datetime.timedelta(days=1)
+ct_1 = datetime.datetime.now() + datetime.timedelta(days=no_days)
+Predict=pd.date_range(ct,ct_1, freq='D')
+future_df = pd.DataFrame(index=Predict)
+future_df['future_energy'] = future_pred_lr.tolist()
+st.write(future_df)
+
+
+
+
+
+
+
+#st.write("Model loaded")
+#result = model.predict(df)
+#st.subheader('Predicted Result')
+#st.write(result)
